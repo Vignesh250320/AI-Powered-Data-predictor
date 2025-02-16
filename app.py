c201d5f70d5b64f7ff51fb5c76f587788c4e3534
Vignesh250320 <omvicky333@gmail.com>
2025-02-16 11:13:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-pip install streamlit pandas scikit-learn
 
 # Streamlit app title
 st.title("AI Powered Data Predictor")
